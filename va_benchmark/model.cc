e875ccdc4f14ed6f45c845a4cf9e1fe3968eb4c2
jamie <neworderofjamie@gmail.com>
2018-11-23 13:06:03 +0000

    Self-contained VA benchmark model

---
+++ va_benchmark/model.cc
@@ -0,0 +1,95 @@
+#include <cmath>
+#include <vector>
+
+// GeNN includes
+#include "modelSpec.h"
+
+// VA benchmark includes
+#include "exp_curr.h"
+#include "lif.h"
+#include "parameters.h"
+
+void modelDefinition(NNmodel &model)
+{
+    initGeNN();
+    model.setDT(1.0);
+    model.setName("va_benchmark");
+
+    GENN_PREFERENCES::autoInitSparseVars = true;
+    GENN_PREFERENCES::defaultVarMode = VarMode::LOC_DEVICE_INIT_DEVICE;
+    GENN_PREFERENCES::defaultSparseConnectivityMode = VarMode::LOC_DEVICE_INIT_DEVICE;
+
+    //---------------------------------------------------------------------------
+    // Build model
+    //---------------------------------------------------------------------------
+    InitVarSnippet::Uniform::ParamValues vDist(
+        Parameters::resetVoltage,       // 0 - min
+        Parameters::thresholdVoltage);  // 1 - max
+
+    InitSparseConnectivitySnippet::FixedProbability::ParamValues fixedProb(
+        Parameters::probabilityConnection); // 0 - prob
+
+    // LIF model parameters
+    GeNNModels::LIF::ParamValues lifParams(
+        1.0,    // 0 - C
+        20.0,   // 1 - TauM
+        -49.0,  // 2 - Vrest
+        Parameters::resetVoltage,  // 3 - Vreset
+        Parameters::thresholdVoltage,  // 4 - Vthresh
+        0.0,    // 5 - Ioffset
+        5.0);    // 6 - TauRefrac
+
+    // LIF initial conditions
+    GeNNModels::LIF::VarValues lifInit(
+        initVar<InitVarSnippet::Uniform>(vDist),     // 0 - V
+        0.0);   // 1 - RefracTime
+
+    // Static synapse parameters
+    WeightUpdateModels::StaticPulse::VarValues excitatoryStaticSynapseInit(
+        Parameters::excitatoryWeight);    // 0 - Wij (nA)
+
+    WeightUpdateModels::StaticPulse::VarValues inhibitoryStaticSynapseInit(
+        Parameters::inhibitoryWeight);    // 0 - Wij (nA)
+
+    // Exponential current parameters
+    GeNNModels::ExpCurr::ParamValues excitatoryExpCurrParams(
+        5.0);  // 0 - TauSyn (ms)
+
+    GeNNModels::ExpCurr::ParamValues inhibitoryExpCurrParams(
+        10.0);  // 0 - TauSyn (ms)
+
+    // Create IF_curr neuron
+    auto *e = model.addNeuronPopulation<GeNNModels::LIF>("E", Parameters::numExcitatory, lifParams, lifInit);
+    auto *i = model.addNeuronPopulation<GeNNModels::LIF>("I", Parameters::numInhibitory, lifParams, lifInit);
+
+    model.addSynapsePopulation<WeightUpdateModels::StaticPulse, GeNNModels::ExpCurr>(
+        "EE", SynapseMatrixType::RAGGED_GLOBALG, NO_DELAY,
+        "E", "E",
+        {}, excitatoryStaticSynapseInit,
+        excitatoryExpCurrParams, {},
+        initConnectivity<InitSparseConnectivitySnippet::FixedProbabilityNoAutapse>(fixedProb));
+    model.addSynapsePopulation<WeightUpdateModels::StaticPulse, GeNNModels::ExpCurr>(
+        "EI", SynapseMatrixType::RAGGED_GLOBALG, NO_DELAY,
+        "E", "I",
+        {}, excitatoryStaticSynapseInit,
+        excitatoryExpCurrParams, {},
+        initConnectivity<InitSparseConnectivitySnippet::FixedProbability>(fixedProb));
+    model.addSynapsePopulation<WeightUpdateModels::StaticPulse, GeNNModels::ExpCurr>(
+        "II", SynapseMatrixType::RAGGED_GLOBALG, NO_DELAY,
+        "I", "I",
+        {}, inhibitoryStaticSynapseInit,
+        inhibitoryExpCurrParams, {},
+        initConnectivity<InitSparseConnectivitySnippet::FixedProbabilityNoAutapse>(fixedProb));
+    model.addSynapsePopulation<WeightUpdateModels::StaticPulse, GeNNModels::ExpCurr>(
+        "IE", SynapseMatrixType::RAGGED_GLOBALG, NO_DELAY,
+        "I", "E",
+        {}, inhibitoryStaticSynapseInit,
+        inhibitoryExpCurrParams, {},
+        initConnectivity<InitSparseConnectivitySnippet::FixedProbability>(fixedProb));
+
+    // Configure spike variables so that they can be downloaded to host
+    e->setSpikeVarMode(VarMode::LOC_HOST_DEVICE_INIT_DEVICE);
+    i->setSpikeVarMode(VarMode::LOC_HOST_DEVICE_INIT_DEVICE);
+
+    model.finalize();
+}
---
+++ va_benchmark/simulator.cc
@@ -0,0 +1,34 @@
+// VA benchmark includes
+#include "parameters.h"
+#include "spike_csv_recorder.h"
+
+// Auto-generated model code
+#include "va_benchmark_CODE/definitions.h"
+
+using namespace BoBRobotics;
+
+int main()
+{
+    allocateMem();
+    initialize();
+    initva_benchmark();
+
+    // Open CSV output files
+    SpikeCSVRecorder spikes("spikes.csv", glbSpkCntE, glbSpkE);
+
+    // Loop through timesteps
+    while(t < 10000.0f) {
+        // Simulate
+#ifndef CPU_ONLY
+        stepTimeGPU();
+
+        pullECurrentSpikesFromDevice();
+#else
+        stepTimeCPU();
+#endif
+
+        spikes.record(t);
+    }
+
+    return EXIT_SUCCESS;
+}
